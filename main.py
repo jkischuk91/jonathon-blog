6c9d82c775d160226c63476c9f015fb91b9f68f3
Jonathon Kischuk <jonathonkischuk91@gmail.com>
2022-06-23 04:23:36 -0400

    sqlite to postgres

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from functools import wraps
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('DATABASE_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -22,7 +22,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 blog_posts = SQLAlchemy(app)
 
